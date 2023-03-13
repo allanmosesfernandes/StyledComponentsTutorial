19ba89e47af898e8e9faea31389f336118dfe20d
Allan Fernandes <Fernzallan14@gmail.com>
2023-03-13 13:40:10 +0000

    Styled Components Init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import Header from "./components/Header/Header.components";
 
+import { Container } from "./components/Styles/container.styles";
 function App() {
   return (
-    <div className="App">
-      <h3>Hello World</h3>
-    </div>
+    <>
+      <Header></Header>
+      <Container>
+        <h3>Hello World</h3>
+      </Container>
+    </>
+
   );
 }
 
---
+++ src/components/Header/Header.components.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyledHeader } from './header.styles'
+
+const Header = () => {
+  return (
+    <StyledHeader>
+        <h2>Header</h2>
+    </StyledHeader>
+  )
+}
+
+export default Header
